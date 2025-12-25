ed3cf1d1ee3c1ba9dc476c5d6d42cdf395e5978e
forzeo677 <contact@forzeo.com>
2025-12-25 09:21:13 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/forzeo/brandanalytics/BrandanalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.forzeo.brandanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandanalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandanalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forzeo/brandanalytics/BrandanalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forzeo.brandanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandanalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
